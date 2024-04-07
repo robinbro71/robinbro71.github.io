56679c92f8a5872ea29d02d2c72d96c149615d69
robinbro71 <165838816+robinbro71@users.noreply.github.com>
2024-04-07 21:33:17 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,2 +1,19 @@
 const namaSaya = "robin tobin";
+let usia = 22;
+
+
+
+function generateBiodata() {
+    if(usia == 22) {
+
+        // INI ADALAH KONDISI PERTAMA
+    }else {
+        // INI ADALAH JIKA KINDISI TIDAK TERPENUHI
+    }
+    console.log(`nama saya adalah ${namaSaya} dan usia saya adalah ${usia} tahun`);
+}
+
 console.log(namaSaya);
+console.log(usia);
+
+generateBiodata();
